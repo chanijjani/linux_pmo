099449fe010ced2c879641b2e94f2405c59cd4f0
Chanhee Lee <chanijjani@gmail.com>
2025-02-20 11:19:01 -0500

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 109 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#109 SMP Mon Feb 17 20:45:28 EST 2025"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "pmemMachine1"
+#define LINUX_COMPILER "gcc (GCC) 11.5.0 20240719 (Red Hat 11.5.0-2), GNU ld version 2.35.2-42.el9_3.1"
